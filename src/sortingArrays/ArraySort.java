6bf19a0a8b81db24d331474d55e64d9749831b78
rshah120 <rshah1@macalester.edu>
2019-11-12 19:45:30 -0600

    Breakout Game Part 3. Preceptor Kyaw Za approved of the ball collisions with the bricks.

---
+++ src/sortingArrays/ArraySort.java
@@ -0,0 +1,41 @@
+package sortingArrays;
+
+import java.util.Arrays;
+
+public class ArraySort {
+
+    private static String[] merge(String[] a, String[] b) {
+        int newLength = a.length + b.length;
+
+        String[] merged = new String[newLength];
+        for (int i = 0, ai = 0, bi = 0; i < newLength; i++) {
+            System.out.println(ai + a.length + bi + b.length);
+            if (a[ai].compareTo(b[bi]) < 0) {
+                merged[i] = a[ai];
+                ai++;
+            } else {
+                merged[i] = b[bi];
+                bi++;
+            }
+        }
+        return merged;
+    }
+
+    public static String[] sort(String[] a) {
+        if (a == null) {
+            return null;
+        }
+
+        if (a.length == 1 || a.length == 0) {
+            return a;
+        }
+
+        int mid = a.length / 2;
+
+        String[] front = Arrays.copyOfRange(a, 0, mid);
+        String[] back = Arrays.copyOfRange(a, mid, a.length);
+
+        return merge(sort(front), sort(back));
+    }
+
+}
